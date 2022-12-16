5e9760f80b9467158c04c48e1551b5876d26968e
BlackBeltHacker <114818497+BlackBeltHacker@users.noreply.github.com>
2022-12-16 14:32:54 +0000

    some new stuff

--- js/app.js
+++ js/app.js
@@ -4,15 +4,23 @@ var itemWrapper = document.querySelector('main');
 function displayMatches(matches) {
   itemWrapper.innerHTML = '';
 
-  for (var matchObj of matches) {
-    itemWrapper.insertAdjacentHTML('beforeend', `
-      <div class="movie-item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${matchObj.Poster})">
-        <h3>${matchObj.Title}</h3>
-        <p>Release Year: ${matchObj.Year}</p>
-        <a href="https://www.imdb.com/title/${matchObj.imdbID}" target="_blank">View More Details</a>
-      </div>
-    `);
+  if (!matches) {
+
+  } else {
+    
+    for (var matchObj of matches) {
+      itemWrapper.insertAdjacentHTML('beforeend', `
+        <div class="movie-item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${matchObj.Poster})">
+          <h3>${matchObj.Title}</h3>
+          <p>Release Year: ${matchObj.Year}</p>
+          <a href="https://www.imdb.com/title/${matchObj.imdbID}" target="_blank">View More Details</a>
+        </div>
+      `);
+    }
+
   }
+
+  
 }
 
 function getMovieData(event) {
